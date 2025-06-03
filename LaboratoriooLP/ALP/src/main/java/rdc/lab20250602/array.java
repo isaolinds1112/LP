673f7d7213a8b70cd0dcbf0bb93180d53f6b34d0
aluno <aluno@5A445JW1Z>
2025-06-03 09:13:01 -0300

    Atividade fila 02/06/2025

---
+++ LaboratoriooLP/ALP/src/main/java/rdc/lab20250602/array.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rdc.lab20250602;
+
+/**
+ *
+ * @author aluno
+ */
+public class array {
+    
+}
